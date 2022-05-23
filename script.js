0f414bd536f2ca47c5c431cfa7f9f7c733ccbd4e
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-05-23 00:09:13 -0500

    se formateo el codigo de la pagina web

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 console.log("script funcionando");
 
- /*Creacion del mensaje a mostrar en un correo equivocado*/ 
- const p = document.createElement("p");
- p.id = "advertencia";
- 
- /*Posicon en donde se mostrara el mensaje*/
+/*Creacion del mensaje a mostrar en un correo equivocado*/
+const p = document.createElement("p");
+p.id = "advertencia";
+
+/*Posicon en donde se mostrara el mensaje*/
 const frm = document.querySelector(".formulario");
 
+document.getElementById("btn").addEventListener("click", () => {
+    console.log("se presiono el boton");
+    let correo = document.getElementById("entrada").value;
 
-document.getElementById("btn").addEventListener("click", ()=>{
- console.log("se presiono el boton");
- let correo = document.getElementById("entrada").value;
- 
-validarCorreo(correo);
-})
+    validarCorreo(correo);
+});
 
-
-function validarCorreo(email){
-    let expresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (expresion.test(email)){
-        p.textContent=" ";   
-    } 
-    else 
-    { 
-        p.textContent="inserta un correo valido!";
+function validarCorreo(email) {
+    let expresion =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (expresion.test(email)) {
+        p.textContent = " ";
+    } else {
+        p.textContent = "inserta un correo valido!";
     }
-    frm.insertAdjacentElement("afterend",p);
+    frm.insertAdjacentElement("afterend", p);
 }
